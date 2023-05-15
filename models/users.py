706770c2eb2db1baae68e74c8a62172c226a5403
김경민 <dev.kkim@gmail.com>
2023-05-15 21:22:48 +0900

    NO database application

--- models/users.py
+++ models/users.py
@@ -1,9 +1,26 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional, List
-from events import Event
+from models.events import Event
 
 class User(BaseModel):
     email: EmailStr
     password: str
     events: Optional[List[List]]
-    
+
+    class config:
+        schema_extra = {
+            "example": "fastapi@packt.com",
+            "username": "strong!!",
+            "events": []
+        }
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class config:
+        schema_extra = {
+            "example": "fastapi@packt.com",
+            "password": "string!!",
+            "events": [],
+        }
